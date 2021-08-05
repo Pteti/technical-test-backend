c01142f2c0099a771e4e82b914f240dde1afd03f
Peter Gellert <pteti95@gmail.com>
2021-08-05 22:04:48 +0200

    Dummy requests in controller

--- src/main/java/com/playtomic/tests/wallet/api/WalletController.java
+++ src/main/java/com/playtomic/tests/wallet/api/WalletController.java
@@ -2,8 +2,10 @@ package com.playtomic.tests.wallet.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 @RestController
 public class WalletController {
@@ -13,4 +15,22 @@ public class WalletController {
     void log() {
         log.info("Logging from /");
     }
+
+    @GetMapping("/wallet/{id}")
+    long getBalance(@PathVariable("id") long walletId) {
+        log.info("Requesting wallet balance for wallet: " + walletId);
+        return 10l;
+    };
+
+    @PostMapping("/wallet/{id}/charge/{amount}")
+    long chargeWallet(@PathVariable("id") long walletId, @PathVariable("amount") long amount) {
+        log.info("charging wallet.");
+        return 10l;
+    };
+
+    @PostMapping("/wallet/{id}/topup/{amount}")
+    long topUpWallet(@PathVariable("id") long walletId, @PathVariable("amount") long amount){
+        log.info("top up wallet.");
+        return 10l;
+    };
 }
